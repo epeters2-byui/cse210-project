2979a26520ed4d9798d27b1a9e6b850c28869cae
epeters2 <epeters2@byupathway.edu>
2025-11-20 02:36:53 -0700

    week01 assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // For part 1 and 2
+        Console.WriteLine("What is your magic number?  ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // For part 3 use random number
+       // Random randomGenerator = new Random();
+       // int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else
+            {
+                Console.WriteLine("You guess it! ");
+            }
+        }
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1. _jobTitle = "Computer Engineer";
+        job1._company = "LTA";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Telecom";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "IT Lecturer";
+        job3. _company = " United Methodist University";
+        job3._startYear = 2023;
+        job3._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emmanuel M. Peters";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
+
     }
 }
